0acdadac90cd24c76f9d231578a50842325d6fe6
shivamtiwari1230 <shivamtiwari1234550@gmail.com>
2025-04-05 03:35:41 +0530

    understanding callbacks

---
+++ callback-fun/index.js
@@ -0,0 +1,36 @@
+function printMessage() {
+  console.log("Mujhe callback function ke through call kiya gaya!");
+}
+
+function executeCallback(cb) {
+  cb(); // Yahan jo function pass hoga, use call kar diya
+}
+
+executeCallback(printMessage);
+
+function add(a, b) {
+  return a + b;
+}
+
+function cb(callback) {
+  return callback(2, 4)
+}
+console.log(cb(add)
+);
+
+
+const square = (num) => num * num;
+
+const nums = [2,4,6]
+function myFn(callback) {
+    const squareArr = []
+    for(let i = 0; i < nums.length; i++){
+        let res = callback(nums[i])
+        squareArr.push(res)
+    }
+    return squareArr
+}
+
+console.log(myFn(square));
+
+

e0ee0cbf95058a845a680e50dbb00a1e80b4c429
shivamtiwari1230 <shivamtiwari1234550@gmail.com>
2025-04-16 02:36:46 +0530

    closures

--- closure/index.js
+++ closure/index.js
@@ -1,17 +1,56 @@
-function outer(){
-    let username = "hitesh"
+function outer() {
+  let username = "hitesh";
 
-    function inner(){
-        console.log("inner", username);
-        
-    }
+  function inner() {
+    console.log("inner", username);
+  }
 
-    function innerTwo(){
-        console.log("innerTwo", username);
-        
-    }
+  function innerTwo() {
+    console.log("innerTwo", username);
+  }
 
-    inner()
-    innerTwo()
+  inner();
+  innerTwo();
 }
-outer()
+outer();
+
+// -----------------------------------------------------
+// closure
+
+function makeFunc() {
+  const name = "Aman";
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+
+const myFunc = makeFunc();
+myFunc();
+
+// ----------------------------------------------------
+
+function createCounter() {
+  let count = 0; // This variable is "private"
+
+  return {
+    increment: function () {
+      count++;
+      return count;
+    },
+
+    decrement: function () {
+      count--;
+      return count;
+    },
+
+    getCount: function(){
+        return count
+    }
+  };
+}
+
+const myCounter = createCounter();
+console.log(myCounter.increment());
+console.log(myCounter.decrement());
+

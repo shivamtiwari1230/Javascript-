7e15351cc61c91a30c4d04a34b4bbd61b884520e
shivamtiwari1230 <shivamtiwari1234550@gmail.com>
2025-04-19 17:22:34 +0530

    class property

--- fun-constructors/index.js
+++ fun-constructors/index.js
@@ -1,14 +1,31 @@
-function Person(fname, lname, contact) {
-  this.fname = fname;
-  this.lname = lname;
-  this.contact = contact;
+// function Person(fname, lname, contact) {
+//   this.fname = fname;
+//   this.lname = lname;
+//   this.contact = contact;
 
-  this.getName = function () {
-    console.log(this.fname, this.lname);
-  };
+//   this.getName = function () {
+//     console.log(this.fname, this.lname);
+//   };
+// }
+
+// const person1 = new Person('Piyush', 'Garg', '98989898')
+// console.log(person1);
+
+// person1.getName();
+
+
+
+class Person {
+    constructor(fname, lname, contact) {
+        this.fname = fname
+        this.lname = lname
+        this.contact = contact
+    }
+    getName(){
+        console.log(this.fname, this.lname);
+        
+    }
 }
 
-const person1 = new Person('Piyush', 'Garg', '98989898')
-console.log(person1);
-
-person1.getName();
+const p1 = new Person ('Aman', 'Gupta', '989898')
+p1.getName()

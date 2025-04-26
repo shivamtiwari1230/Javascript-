68bd1eba10e7b14213d8ad9d767a1cccd7c704d8
shivamtiwari1230 <shivamtiwari1234550@gmail.com>
2025-04-26 23:46:26 +0530

    oops

--- oops/index.js
+++ oops/index.js
@@ -1,15 +1,42 @@
+function createCounter() {
+  let count = 0;
 
-function createCounter (){
-    let count = 0
-
-    return function increment (){
-        count++
-        return count
-    }
+  return function increment() {
+    count++;
+    return count;
+  };
 }
 
-const counter = createCounter()
+const counter = createCounter();
 console.log(counter());
 
+// -------------------------------------------------------------------------------------
+
+// Inheritance
+
+class vehicle {
+  constructor(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+
+  displayInfo() {
+    console.log(`${this.year} ${this.brand}`);
+  }
+}
+
+const newVehicle = new vehicle("Hero", 2020, 2021);
+// 
+// Child class (Car)
+
+let obj = {
+  name: "JS",
+  getName: function () {
+    return this.name;
+  },
+};
+
+console.log(obj.getName());
 
 

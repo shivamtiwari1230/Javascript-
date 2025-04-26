9fd931b7c1c5d953db005ed5ad72270c4276f539
shivamtiwari1230 <shivamtiwari1234550@gmail.com>
2025-04-26 23:45:38 +0530

    oops

--- fun-constructors/index1.js
+++ fun-constructors/index1.js
@@ -1,15 +1,17 @@
-// class Person {
-//     constructor(name, age){
-//         this.name = name;
-//         this.age = age;
-//     }
-// }
-// Person.prototype.greet = function () {
-//     return `Hello, My name is ${this.name}`
-// };
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+Person.prototype.greet = function () {
+    return `Hello, My name is ${this.name}`
+};
 
-// const user  = new Person("Aman", 21);
-// console.log(user.greet());
+const user  = new Person("Aman", 21);
+console.log(user.greet());
+
+// ---------------------------------------------------------
 
 function Person1(name, age) {
   this.name = name;
